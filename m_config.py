74528e1f55d93a30281d72c44113455d9ba95bb9
Matt Sutherland <47634034+sudz4@users.noreply.github.com>
2022-11-28 14:55:20 -0800

    Update m_config.py

--- m_config.py
+++ m_config.py
@@ -1,5 +1,5 @@
-IEX_API_KEY = 'pk_565144def319c4ee6bfd15243a75b0caf' # Legacy endpoints
+IEX_API_KEY = 'pk_56514def319c4ee6bfd15243a75b0caf' # Legacy endpoints
 
-OPENAI_API_KEY = 'sk-Zy8f2azFO744furxkoIqrT3BlbkFJ12NPwF3VPKpTd5gcRlCj'
+OPENAI_API_KEY = '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'
 
-IEX_API_KEY_NEW = 'sk_b128e0373e65441f8824a29d1e507461a' # New IEX endpoints
+IEX_API_KEY_NEW = 'sk_b128e0373e6541f8824a29d1e507461a' # New IEX endpoints
