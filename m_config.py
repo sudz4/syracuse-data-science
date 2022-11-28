d42dbee22b3be2e78653ef80999fcce4f4d6df11
Matt Sutherland <mattsutherland@machine.attlocal.net>
2022-11-27 18:00:22 -0800

    initial commit

---
+++ m_config.py
@@ -0,0 +1,5 @@
+IEX_API_KEY = 'pk_56514def319c4ee6bfd15243a75b0caf' # Legacy endpoints
+
+OPENAI_API_KEY = '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'
+
+IEX_API_KEY_NEW = 'sk_b128e0373e6541f8824a29d1e507461a' # New IEX endpoints
