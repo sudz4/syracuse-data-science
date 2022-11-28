298bf17942908babfd912ac9b40c8d92596412c2
Matt Sutherland <47634034+sudz4@users.noreply.github.com>
2022-11-28 15:02:31 -0800

    Delete m_config.py

--- m_config.py
+++
@@ -1,5 +0,0 @@
-IEX_API_KEY = 'pk_565144def319c4ee6bfd15243a75b0caf' # Legacy endpoints
-
-OPENAI_API_KEY = 'sk-Zy8f2azFO744furxkoIqrT3BlbkFJ12NPwF3VPKpTd5gcRlCj'
-
-IEX_API_KEY_NEW = 'sk_b128e0373e65441f8824a29d1e507461a' # New IEX endpoints
